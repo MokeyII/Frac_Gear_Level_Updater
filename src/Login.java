22bd08dc9f6ae8171bea28c29e84ac9a9cd7e5d6
George McClain <McClainG9616@gmail.com>
2018-04-13 23:48:37 -0400

    initial upload

---
+++ src/Login.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.SimpleTimeZone;
+
+public class Login {
+    private JTextField txtUsername;
+    private JPasswordField pwdPassword;
+    private JButton btnLogin;
+    private JLabel lblUsername;
+    private JLabel lblPassword;
+    private JPanel loginPanel;
+
+
+    public Login() {
+        btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                String Uname = "";
+                String PWD = "";
+
+                Uname = txtUsername.getText();
+                PWD = pwdPassword.toString();
+
+                if (Uname == "FUCKJAVA" && PWD == "FUCKME")
+                {
+                   JOptionPane.showMessageDialog(null, "This Seems to be working!!!");
+                }
+                else
+                {
+                   JOptionPane.showMessageDialog(null,"You Done F***ed up boy");
+                }
+
+
+            }
+        });
+    }
+
+    public static void main(String[] args)  {
+        JFrame loginFrame = new JFrame("LoginApp");
+        loginFrame.setContentPane(new Login().loginPanel);
+        loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        loginFrame.pack();
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        loginFrame.setLocation(dim.width/2-loginFrame.getSize().width/2, dim.height/2-loginFrame.getSize().height/2);
+        loginFrame.setVisible(true);
+    }
+
+}
